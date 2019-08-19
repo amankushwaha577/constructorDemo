eb2842224177b4f887aabf8dfc1363eb48733947
amankushwaha577 <aman.kushwaha_cs18@gla.ac.in>
2019-08-19 15:50:41 +0530

    Initial commit

---
+++ src/main/ConstructorDemo.java
@@ -0,0 +1,13 @@
+package main;
+
+public class ConstructorDemo {
+    public static void main(String[] args) {
+        private int number;
+        private String name;
+
+        public int getNumber()
+        {
+
+        }
+    }
+}
